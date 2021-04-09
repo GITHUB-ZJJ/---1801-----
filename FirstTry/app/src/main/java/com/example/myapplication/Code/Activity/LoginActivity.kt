09a63a6ef203502b012d93bdace3a6b345b0dfb6
物联网1801-钟锦基-20184324 <67481077+GITHUB-ZJJ@users.noreply.github.com>
2021-04-09 15:08:54 +0800

    第二天

--- FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/LoginActivity.kt
+++ FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.myapplication
+package com.example.myapplication.Code.Activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import com.example.myapplication.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
--- FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/RegisterActvity.kt
+++ FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/RegisterActvity.kt
@@ -1,9 +1,11 @@
-package com.example.myapplication
+package com.example.myapplication.Code.Activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import com.example.myapplication.R
 
-class RegisterActvity : AppCompatActivity() {
+class RegisterActvity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
