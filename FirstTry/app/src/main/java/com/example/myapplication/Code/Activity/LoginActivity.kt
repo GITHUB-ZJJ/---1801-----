a2c144298a8b34f4484a01c8bee1905c59c92671
物联网1801-钟锦基-20184324 <67481077+GITHUB-ZJJ@users.noreply.github.com>
2021-04-09 17:33:27 +0800

    第二天第二次

--- FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/LoginActivity.kt
+++ FirstTry/app/src/main/java/com/example/myapplication/Code/Activity/LoginActivity.kt
@@ -2,9 +2,10 @@ package com.example.myapplication.Code.Activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
-class LoginActivity : Activity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
